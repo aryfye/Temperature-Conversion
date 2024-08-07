3cc6617e8821b56ff068932225faa6fe11b42ff0
aryan <aryanlakhani2001@gmail.com>
2024-08-07 19:21:43 -0400

    Uploading files

---
+++ weather.js
@@ -0,0 +1,21 @@
+// get references to the elements
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+
+// function to convert temperatures
+function convert() {
+    let temp = Number(textBox.value);
+
+    if (toFahrenheit.checked) {
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    } else if (toCelsius.checked) {
+        temp = (temp - 32) * 5 / 9;
+        result.textContent = temp.toFixed(1) + "°C";
+    } else {
+        result.textContent = "Select a unit";
+    }
+}
